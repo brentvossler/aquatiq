465ed72af072af9846219f24c8a8158e8fecbf3c
Brent Vossler <brentvossler@Brents-MacBook-Pro.local>
2016-10-20 20:41:43 -0600

    weather API added

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Angular setup app with ngRoute dependency and controllers
 angular.module("mainApp", ["ngRoute"])
-	.controller("mainCtrl", mainController);
+	.controller("mainCtrl", mainController)
+	.controller("weatherAPI", weatherController);
 
 // Front end router
 angular.module('mainApp')
@@ -30,33 +31,44 @@ function mainController() {
     var main = this;
     window.main = main;
 
-    main.greeting = "I'M ON A BOAT!";
-
-
     main.addBoat = function() {
     	console.log("Adding a boat!");
-
     	var newBoat = {
     		title: main.title,
     		description: main.description,
+    		capacity: main.capacity,
+    		price: main.price,
     	}
 
     	main.list.push(newBoat);
     }
 
-    main.list = [
-	    {
-	    	title: "21ft powerboat",
-	    	description: "this is an amazing, fast, super-duper hotrod boat!.",
-	    	image: "pull cloudinary images in...",
-	    	price: "$150.00",
-	    	location: "San Diego, CA - San Diego Bay"
-	    }
-    ]
+    main.list = []
 }
 
 // Start Second Controller
-function secondController () {
-	var second = this;
-	second.greeting = "second text second text";
+weatherController.$inject = ['$http'];
+// API KEY: 1db076fb994e59375b4ec4176b93391a 
+
+function weatherController($http) {
+	var weather = this;
+	weather.forecast = {};
+	
+	weather.getWeather = function() {
+		$http.get('http://api.openweathermap.org/data/2.5/weather?zip={80306},{us}&units=imperial&APPID=1db076fb994e59375b4ec4176b93391a').then(
+        function(res, status){
+            console.log("got it", res.data);
+            weather.forecast = {
+            	name: res.data.name,
+            	skies: res.data.weather[0].main,
+            	icon: res.data.weather[0].icon,
+            	temp: res.data.main.temp,
+            	wind: res.data.wind.speed
+            }  
+        },
+        function(res, status){
+            console.log("GET weather failure", status);
+        });
+	}
+	weather.getWeather();
 }
